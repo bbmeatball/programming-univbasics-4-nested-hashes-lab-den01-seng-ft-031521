52fc443938228d0274fffa4b1ac4b50bbae6de29
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-01-31 18:35:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = "GUI"
+    return programmer_hash
 end
 
 def adding_to_dennis
@@ -131,8 +132,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
  		}
-    programmer_hash[:alan_kay][:known_for] = "GUI"
-    return programmer_hash
+    
  		
   
 end
