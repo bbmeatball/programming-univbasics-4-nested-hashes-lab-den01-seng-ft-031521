9768761f51618d7196c46548328eabc7787e2a87
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-01-31 18:37:16 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,10 +79,16 @@ def adding_matz
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
+      },
+      :yukihiro_matsumoto =>{
+          :known_for => "Ruby",
+          :languages => ["LISP","C"]
       }
-    }
+}
+#{:known_for => "Ruby", :languages => ["LISP","C"]}]
 
 end
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
@@ -103,7 +109,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = "GUI"
+    return programmer_hash
 end
 
 def adding_to_dennis
@@ -124,6 +131,9 @@ def adding_to_dennis
         :known_for => "Unix",
         :languages => ["C"]
       }
-    }
-
+ 		}
+    programmer_hash[:dennis_ritchie][:languages][1]="Assembly"
+    return programmer_hash
+ 		
+  
 end
